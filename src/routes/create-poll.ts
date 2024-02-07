7dfa7b1dc9e02b8400fca694bdd775100be95071
Giovanni Mota de Oliveira <devgiovannimota@gmail.com>
2024-02-07 07:31:54 -0300

    Get poll by id

--- src/routes/create-poll.ts
+++ src/routes/create-poll.ts
@@ -6,10 +6,23 @@ export async function createPoll(app: FastifyInstance) {
   app.post("/polls", async (request, reply) => {
     const createPollBody = z.object({
       title: z.string(),
+      options: z.array(z.string()),
     });
 
-    const { title } = createPollBody.parse(request.body);
-    const poll = await prisma.poll.create({ data: { title: title } });
-    return poll;
+    const { title, options } = createPollBody.parse(request.body);
+    const poll = await prisma.poll.create({
+      data: {
+        title,
+        options: {
+          createMany: {
+            data: options.map((option) => {
+              return { title: option };
+            }),
+          },
+        },
+      },
+    });
+
+    return reply.status(201).send({ pollid: poll.id });
   });
 }
